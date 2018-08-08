1aa0d79507d9c7148957c73755d9cc10c29b4c64
mario.andujar <mario.andujar@etoolinnovation.com>
2018-08-08 14:06:46 +0200

    FG-221 setup mongo_mapper

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='MongoMapper.python',
+      version='0.1',
+      description='ORM MongoDB',
+      url='https://bitbucket.org/etooltech/mongo_mapper',
+      author='ETOOL Innovation',
+      author_email='info@etoolinnovation.com',
+      license='MIT',
+      packages=['mongo_mapper'],
+      zip_safe=False)
