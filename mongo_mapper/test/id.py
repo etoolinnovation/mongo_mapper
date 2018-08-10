28a376b927414d04929ff009f756c09d53548367
Emilio Torrens <emilio.torrens@etoolinnovation.com>
2018-08-10 13:47:09 +0200

    if test fix

--- mongo_mapper/test/id.py
+++ mongo_mapper/test/id.py
@@ -1,4 +1,13 @@
 from mongo_mapper.core.id_manager import IdType, get_id
+from mongo_mapper import config
+
+config.load_config([
+        {
+            "ALIAS": "default",
+            'URL': 'mongodb://10.0.1.155/',
+            'DB_NAME': 'test-mongo_mapper'
+        }
+    ])
 
 print(get_id(IdType.Numeric, None))
 print(get_id(IdType.Incremental, 'Hotel'))
