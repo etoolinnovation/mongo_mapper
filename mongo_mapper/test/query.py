120dc60f2fbf6cad00369cfad663bf3751048184
Emilio Torrens <emilio.torrens@etoolinnovation.com>
2018-08-10 12:35:25 +0200

    FG-221 MongoMapper v.5 FG-226 Creación clase Query

--- mongo_mapper/test/query.py
+++ mongo_mapper/test/query.py
@@ -4,5 +4,5 @@ print(Query.eq("hotel_code", "%1%"))
 print(Query.eq("hotel_code", "1%"))
 print(Query.eq("hotel_code", "%1"))
 
-print(Query.add_and(Query.eq("hotel_code", "H1"), Query.gt("value", 1000), Query.ne("category", "5*")))
+print(Query.add_and(Query.eq("hotel_code", "%100%"), Query.gt("value", 1000), Query.ne("category", "5*")))
 
